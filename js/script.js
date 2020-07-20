446d8a1faf6e9eaadad43a60af61393dddf7bbd8
Karolina Marek <karolinaw1211@gmail.com>
2020-07-20 19:26:52 +0200

    refactored code

--- js/script.js
+++ js/script.js
@@ -1,35 +1,44 @@
-console.log("Cześć");
+{
+    const welcome = () => {
+        console.log("Cześć");
+    };
 
-let button = document.querySelector(".section__button--delete");
-let image = document.querySelector(".section__image");
+    const button = document.querySelector(".section__button--delete");
 
-button.addEventListener("click", () => {
+    const removeImage = () => {
+        const image = document.querySelector(".section__image");
 
-    image.classList.toggle("js-section__withoutImage");
+        image.classList.toggle("js-section__withoutImage");
+        if (image.classList.contains("js-section__withoutImage")) {
+            button.innerHTML = "Przywróć zdjęcie";
+        } else {
+            button.innerHTML = "Usuń zdjęcie";
+        }
+    };
 
-    if (image.classList.contains("js-section__withoutImage")) {
-        button.innerHTML = "Przywróć zdjęcie";
+    const init = () => {
+        button.addEventListener("click", removeImage);
 
-    } else {
-        button.innerHTML = "Usuń zdjęcie";
+        welcome();
+    };
 
-    }
+    init();
+}
 
-});
+{
+    const check = document.querySelector(".table__cell--check");
 
+    const checkTrack = () => {
 
+        check.classList.toggle(".table__imageItem--yes");
 
-let check = document.querySelector(".table__cell--check");
+        if (check.classList.contains(".table__imageItem--yes")) {
+            check.innerHTML = "<img class='table__imageItem--yes' src='https://cdn.pixabay.com/photo/2012/04/16/12/28/mark-35780_960_720.png' alt='Niezaliczony'>"
+        } else {
+            check.innerHTML = "<img class='table__imageItem' src='https://cdn.pixabay.com/photo/2013/07/12/12/44/cancel-146131_960_720.png' alt='Niezaliczony'>"
+        }
+    };
 
-check.addEventListener("click", () => {
+    check.addEventListener("click", checkTrack);
 
-    check.classList.toggle(".table__imageItem--yes");
-
-    if (check.classList.contains(".table__imageItem--yes")) {
-        console.log('TU DZIALA');
-        check.innerHTML = "<img class='table__imageItem--yes' src='https://cdn.pixabay.com/photo/2012/04/16/12/28/mark-35780_960_720.png' alt='Niezaliczony'>"
-    } else {
-        check.innerHTML = "<img class='table__imageItem' src='https://cdn.pixabay.com/photo/2013/07/12/12/44/cancel-146131_960_720.png' alt='Niezaliczony'>"
-    }
-
-});
+}
